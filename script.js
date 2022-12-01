757a2066511d4d248c44314baf815cfa4af81d35
aman kaushik <kaushikaman239@gmail.com>
2022-11-30 17:28:07 -0800

    add-pre-loader

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ menuIcon.onclick = function(){
     sidebar.classList.toggle("slider")
     container.classList.toggle("large-container")
 }
+
+
+ /* ----page-loader---- */
+
+var preLoader = document.querySelector("#pageloader");
+function myfunction(){
+    preLoader.style.display="none";
+}
