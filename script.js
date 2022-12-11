4ed9ce2924d1ef647b277f15f4e3594c4824dd6c
aman kaushik <kaushikaman239@gmail.com>
2022-12-11 09:42:20 -0800

    add-on

--- script.js
+++ script.js
@@ -18,15 +18,15 @@ menuIcon.onclick = function(){
 
 // ----STICKY NAV----
 
-num = 50,
+// num = 50,
 
-$(window).bind('scroll',function (){
-   if($(window).scrollTop()>num ) {
-    $('header').addClass('fixed');
-   }else{
-    $('header').removeClass('fixed');
-   }
-});
+// $(window).bind('scroll',function (){
+//    if($(window).scrollTop()>num ) {
+//     $('header').addClass('fixed');
+//    }else{
+//     $('header').removeClass('fixed');
+//    }
+// });
 
 
 $('.banner').slick({
@@ -36,4 +36,53 @@ $('.banner').slick({
     arrows: false, 
     dots: true,
     speed: 500,
+});
+
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
+
+
+// ----dropdown----
+
+$('.dropdown img').on('click',function(){
+    $('dropmenu a').slideToggle(280);
+  });
+  
+
+  var colors = ["red" , "green", "blue"]
+for (let color of colors) {
+  
+  (colors);
+}
+
+//   ----file choosen---
+
+const fileUploader = document.getElementById('dropmenu');
+
+fileUploader.addEventListener('change', (event) => {
+  const files = event.target.files;
+  console.log('files', files);
+  
+  for (const file of files) {
+    const name = file.name;
+    const type = file.type ? file.type : 'NA';
+    const size = file.size;
+    const lastModified = file.lastModified;
+    console.log({file, name, type, size, lastModified});
+    
+    const feedback = document.getElementById('feedback');
+    const msg = ` File Name: ${name} <br/>
+              File Size: ${size} <br/>
+              File type: ${type} <br/>
+              File Last Modified: ${new Date(lastModified)}`;
+              
+    feedback.innerHTML = msg;
+  }
 });
