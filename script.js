0abff0c13bb9a85a6b15ff00a39ead2d0a54e530
aman kaushik <kaushikaman239@gmail.com>
2022-12-16 03:08:04 -0800

    add-video

--- script.js
+++ script.js
@@ -64,25 +64,55 @@ for (let color of colors) {
 
 //   ----file choosen---
 
-const fileUploader = document.getElementById('dropmenu');
+// const fileUploader = document.getElementById('dropmenu');
 
-fileUploader.addEventListener('change', (event) => {
-  const files = event.target.files;
-  console.log('files', files);
+// fileUploader.addEventListener('change', (event) => {
+//   const files = event.target.files;
+//   console.log('files', files);
   
-  for (const file of files) {
-    const name = file.name;
-    const type = file.type ? file.type : 'NA';
-    const size = file.size;
-    const lastModified = file.lastModified;
-    console.log({file, name, type, size, lastModified});
+//   for (const file of files) {
+//     const name = file.name;
+//     const type = file.type ? file.type : 'NA';
+//     const size = file.size;
+//     const lastModified = file.lastModified;
+//     console.log({file, name, type, size, lastModified});
     
-    const feedback = document.getElementById('feedback');
-    const msg = ` File Name: ${name} <br/>
-              File Size: ${size} <br/>
-              File type: ${type} <br/>
-              File Last Modified: ${new Date(lastModified)}`;
+//     const feedback = document.getElementById('feedback');
+//     const msg = ` File Name: ${name} <br/>
+//               File Size: ${size} <br/>
+//               File type: ${type} <br/>
+//               File Last Modified: ${new Date(lastModified)}`;
               
-    feedback.innerHTML = msg;
-  }
+//     feedback.innerHTML = msg;
+//   }
+// });
+
+
+
+// video upload
+
+
+const input = document.getElementById('file-input');
+const video = document.getElementById('video');
+const videoSource = document.createElement('source');
+
+input.addEventListener('change', function() {
+  const files = this.files || [];
+
+  if (!files.length) return;
+  
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    videoSource.setAttribute('src', e.target.result);
+    video.appendChild(videoSource);
+    video.load();
+    video.play();
+  };
+  
+  reader.onprogress = function (e) {
+    console.log('progress: ', Math.round((e.loaded * 100) / e.total));
+  };
+  
+  reader.readAsDataURL(files[0]);
 });
